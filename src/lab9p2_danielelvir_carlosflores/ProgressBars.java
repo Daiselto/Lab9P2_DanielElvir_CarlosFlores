1298f458618189efd3cc82f13cb869f0b84f9b8c
Daniel Elvir <danielisrael.elvir@gmail.com>
2023-09-15 16:00:49 -0600

    Avance de thread

--- src/lab9p2_danielelvir_carlosflores/ProgressBars.java
+++ src/lab9p2_danielelvir_carlosflores/ProgressBars.java
@@ -13,6 +13,7 @@ import javax.swing.JProgressBar;
 public class ProgressBars implements Runnable{
     private JProgressBar avan = new JProgressBar();
     private int seg;
+    private int avanz;
     private boolean vive=true;
     private boolean avanzar=true;
 
@@ -23,21 +24,28 @@ public class ProgressBars implements Runnable{
         this.seg = seg;
     }
 
+    public ProgressBars(int seg, int avanz) {
+        this.seg = seg;
+        this.avanz = avanz;
+    }
+    
+    
+
     @Override
     public void run() {
-        /*while(vive){
+        while(vive){
             if(avanzar){
                 avan.setValue(avan.getValue()+1);
-                if(avan.getValue()==avan){
+                if(avan.getValue()==avanz){
                     vive=false;
                 }                
-            } //FIN IF
+            } 
             
             try {
-                Thread.sleep(500);
+                Thread.sleep(seg);
             } catch (InterruptedException ex) {
             }
-        }*/
+        }
     }
     
     
